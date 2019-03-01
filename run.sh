3b73789043ee5413e359370ba4aa3b8ab9a7c5a0
mojtaba <mojtabaeuler@gmail.com>
2019-03-01 15:13:35 +0330

    Test that main proxy is work or not

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec /usr/local/bin/proxy http -t tcp -p :${PORT}
 echo "internal port is => " ${PORT}
+exec /usr/local/bin/proxy http -t tcp -z demo_password -p :${PORT}
 #/bin/sh -c /usr/local/bin/proxy http -t tcp -z demo_password -p :${PORT}
